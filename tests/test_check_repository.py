67b4d415855ef64c0dd581536252eafa3ac8c54c
Pineapple_Juice <st.okopnyi@gmail.com>
2023-08-17 01:54:55 +0700

    hexlet-check

--- tests/test_check_repository.py
+++ tests/test_check_repository.py
@@ -1,55 +1,44 @@
 from collections import namedtuple
 from datetime import datetime
 from pytest import fixture
-
+from unittest import mock
 from page_analyzer.database import PostgresConnection
 from page_analyzer.repository import URLChecksRepository
 
 
+@mock.patch.object(PostgresConnection, "__init__",
+                   new=lambda x: None)
 @fixture
-def checks_repo(mocker):
+def checks_repo():
     repo = URLChecksRepository(PostgresConnection)
     return repo
 
 
-@fixture
-def record():
-    Record = namedtuple(
-            "Record",
-            ["id", "url_id",
-             "status_code", "h1",
-             "title", "description",
-             "created_at"]
-            )
-    return Record(1, 1, 200, "Foo", "Bar", "FooBar", datetime(2023, 8, 16))
+Record = namedtuple(
+    "Record",
+    ["id", "url_id",
+     "status_code", "h1",
+     "title", "description",
+     "created_at"]
+)
+record = Record(1, 1, 200, "Foo", "Bar", "FooBar", datetime(2023, 8, 16))
 
 
-def test_add_check(mocker, checks_repo):
-    mocker.patch("page_analyzer.database.PostgresConnection.__init__",
-                 lambda x: None)
-    mocker.patch("page_analyzer.database.PostgresConnection.execute",
-                 lambda x, y: None)
-
+@mock.patch.object(PostgresConnection, "execute",
+                   new=lambda x, y: None)
+def test_add_check(checks_repo):
     assert checks_repo.add_check(
             1, 200, "Foo", "Bar", "FooBar",
             ) is None
 
 
-def test_get_checks(mocker, checks_repo, record):
-    mocker.patch("page_analyzer.database.PostgresConnection.__init__",
-                 lambda x: None)
-    mocker.patch(
-            "page_analyzer.database.PostgresConnection.execute_and_get_list",
-            lambda x, y: [record,])
-
+@mock.patch.object(PostgresConnection, "execute_and_get_list",
+                   new=lambda x, y: [record,])
+def test_get_checks(checks_repo):
     assert checks_repo.get_checks(1) == [record, ]
 
 
-def test_get_urls_with_checks(mocker, checks_repo, record):
-    mocker.patch("page_analyzer.database.PostgresConnection.__init__",
-                 lambda x: None)
-    mocker.patch(
-            "page_analyzer.database.PostgresConnection.execute_and_get_list",
-            lambda x, y: [record,])
-
+@mock.patch.object(PostgresConnection, "execute_and_get_list",
+                   new=lambda x, y: [record,])
+def test_get_urls_with_checks(checks_repo):
     assert checks_repo.get_urls_with_checks() == [record, ]
--- tests/test_url_repository.py
+++ tests/test_url_repository.py
@@ -1,5 +1,6 @@
 from collections import namedtuple
 from datetime import datetime
+from unittest.mock import patch
 from pytest import fixture
 
 from page_analyzer.database import PostgresConnection
@@ -8,43 +9,32 @@ from page_analyzer.repository import URLRepository
 
 @fixture
 def url_repo(mocker):
+    mocker.patch("page_analyzer.database.PostgresConnection.__init__",
+                 lambda x: None)
     repo = URLRepository(PostgresConnection)
     return repo
 
 
-@fixture
-def record():
-    Record = namedtuple(
-            "Record",
-            ["id", "name", "created_at"]
-            )
-    return Record(1, "https://google.com", datetime(2023, 8, 16))
+Record = namedtuple(
+        "Record",
+        ["id", "name", "created_at"]
+        )
+record = Record(1, "https://google.com", datetime(2023, 8, 16))
 
 
-def test_add(mocker, url_repo):
-    mocker.patch("page_analyzer.database.PostgresConnection.__init__",
-                 lambda x: None)
-    mocker.patch("page_analyzer.database.PostgresConnection.execute",
-                 lambda x, y: None)
-
+@patch.object(PostgresConnection, "execute",
+              lambda x, y: None)
+def test_add(url_repo):
     assert url_repo.add_url("https://google.com") is None
 
 
-def test_get_url(mocker, url_repo, record):
-    mocker.patch("page_analyzer.database.PostgresConnection.__init__",
-                 lambda x: None)
-    mocker.patch(
-            "page_analyzer.database.PostgresConnection.execute_and_get_item",
-            lambda x, y: record)
-
+@patch.object(PostgresConnection, "execute_and_get_item",
+              lambda x, y: record)
+def test_get_url(url_repo):
     assert url_repo.get_url("https://google.com") == record
 
 
-def test_get_id_from_url(mocker, url_repo, record):
-    mocker.patch("page_analyzer.database.PostgresConnection.__init__",
-                 lambda x: None)
-    mocker.patch(
-            "page_analyzer.database.PostgresConnection.execute_and_get_item",
-            lambda x, y: record)
-
+@patch.object(PostgresConnection, "execute_and_get_item",
+              lambda x, y: record)
+def test_get_id_from_url(url_repo):
     assert url_repo.get_id_from_url("https://google.com") == 1
