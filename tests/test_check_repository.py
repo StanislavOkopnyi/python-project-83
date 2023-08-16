4e94e9d3aedeea7e50c08a75ea4f6f7f246028f8
Pineapple_Juice <st.okopnyi@gmail.com>
2023-08-17 00:07:59 +0700

    Add tests for repositories

---
+++ tests/test_check_repository.py
@@ -0,0 +1,48 @@
+from collections import namedtuple
+from datetime import datetime
+from pytest import fixture
+
+from page_analyzer.database import PostgresConnection
+from page_analyzer.repository import URLChecksRepository
+
+@fixture
+def checks_repo():
+    repo = URLChecksRepository(PostgresConnection)
+    return repo
+
+
+@fixture
+def record():
+    Record = namedtuple(
+            "Record",
+            ["id", "url_id",
+             "status_code", "h1",
+             "title", "description",
+             "created_at"]
+            )
+    return Record(1, 1, 200, "Foo", "Bar", "FooBar", datetime(2023, 8, 16))
+
+
+def test_add_check(mocker, checks_repo):
+    mocker.patch("page_analyzer.database.PostgresConnection.execute",
+                 lambda x, y: None)
+
+    assert checks_repo.add_check(
+            1, 200, "Foo", "Bar", "FooBar",
+            ) is None
+
+
+def test_get_checks(mocker, checks_repo, record):
+    mocker.patch(
+            "page_analyzer.database.PostgresConnection.execute_and_get_list",
+            lambda x, y: [record,])
+
+    assert checks_repo.get_checks(1) == [record, ]
+
+    
+def test_get_urls_with_checks(mocker, checks_repo, record):
+    mocker.patch(
+            "page_analyzer.database.PostgresConnection.execute_and_get_list",
+            lambda x, y: [record,])
+
+    assert checks_repo.get_urls_with_checks() == [record, ]
---
+++ tests/test_url_repository.py
@@ -0,0 +1,43 @@
+from collections import namedtuple
+from datetime import datetime
+from pytest import fixture
+
+from page_analyzer.database import PostgresConnection
+from page_analyzer.repository import URLRepository
+
+@fixture
+def url_repo():
+    repo = URLRepository(PostgresConnection)
+    return repo
+
+
+@fixture
+def record():
+    Record = namedtuple(
+            "Record",
+            ["id", "name", "created_at"]
+            )
+    return Record(1, "https://google.com", datetime(2023, 8, 16))
+
+
+
+def test_add(mocker, url_repo):
+    mocker.patch("page_analyzer.database.PostgresConnection.execute", lambda x, y: None)
+
+    assert url_repo.add_url("https://google.com") is None
+
+
+def test_get_url(mocker, url_repo, record):
+    mocker.patch("page_analyzer.database.PostgresConnection.execute_and_get_item",
+                 lambda x, y: record)
+
+    assert url_repo.get_url("https://google.com") == record
+
+def test_get_id_from_url(mocker, url_repo, record):
+    mocker.patch("page_analyzer.database.PostgresConnection.execute_and_get_item",
+                 lambda x, y: record)
+
+    assert url_repo.get_id_from_url("https://google.com") == 1
+
+
+    
