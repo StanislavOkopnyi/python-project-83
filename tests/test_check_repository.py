eeae2133e5e8cab39431d094277dc6448de4fff4
Pineapple_Juice <st.okopnyi@gmail.com>
2023-08-17 14:22:34 +0700

    hexlet-check

--- tests/test_check_repository.py
+++ tests/test_check_repository.py
@@ -1,14 +1,12 @@
 from collections import namedtuple
 from datetime import datetime
 from pytest import fixture
-from unittest import mock
+from unittest.mock import patch
 from page_analyzer.database import PostgresConnection
 from page_analyzer.repository import URLChecksRepository
 
 
 @fixture
-@mock.patch.object(PostgresConnection, "__init__",
-                   new=lambda x: None)
 def checks_repo():
     repo = URLChecksRepository(PostgresConnection)
     return repo
@@ -24,21 +22,27 @@ Record = namedtuple(
 record = Record(1, 1, 200, "Foo", "Bar", "FooBar", datetime(2023, 8, 16))
 
 
-@mock.patch.object(PostgresConnection, "execute",
-                   new=lambda x, y: None)
+@patch.object(PostgresConnection, "execute",
+              new=lambda x, y: None)
+@patch.object(PostgresConnection, "__init__",
+              new=lambda x: None)
 def test_add_check(checks_repo):
     assert checks_repo.add_check(
             1, 200, "Foo", "Bar", "FooBar",
             ) is None
 
 
-@mock.patch.object(PostgresConnection, "execute_and_get_list",
-                   new=lambda x, y: [record,])
+@patch.object(PostgresConnection, "execute_and_get_list",
+              new=lambda x, y: [record,])
+@patch.object(PostgresConnection, "__init__",
+              new=lambda x: None)
 def test_get_checks(checks_repo):
     assert checks_repo.get_checks(1) == [record, ]
 
 
-@mock.patch.object(PostgresConnection, "execute_and_get_list",
-                   new=lambda x, y: [record,])
+@patch.object(PostgresConnection, "execute_and_get_list",
+              new=lambda x, y: [record,])
+@patch.object(PostgresConnection, "__init__",
+              new=lambda x: None)
 def test_get_urls_with_checks(checks_repo):
     assert checks_repo.get_urls_with_checks() == [record, ]
--- tests/test_url_repository.py
+++ tests/test_url_repository.py
@@ -8,8 +8,6 @@ from page_analyzer.repository import URLRepository
 
 
 @fixture
-@patch.object(PostgresConnection, "__init__",
-              new=lambda x: None)
 def url_repo():
     repo = URLRepository(PostgresConnection)
     return repo
@@ -24,17 +22,23 @@ record = Record(1, "https://google.com", datetime(2023, 8, 16))
 
 @patch.object(PostgresConnection, "execute",
               lambda x, y: None)
+@patch.object(PostgresConnection, "__init__",
+              new=lambda x: None)
 def test_add(url_repo):
     assert url_repo.add_url("https://google.com") is None
 
 
 @patch.object(PostgresConnection, "execute_and_get_item",
               lambda x, y: record)
+@patch.object(PostgresConnection, "__init__",
+              new=lambda x: None)
 def test_get_url(url_repo):
     assert url_repo.get_url("https://google.com") == record
 
 
 @patch.object(PostgresConnection, "execute_and_get_item",
               lambda x, y: record)
+@patch.object(PostgresConnection, "__init__",
+              new=lambda x: None)
 def test_get_id_from_url(url_repo):
     assert url_repo.get_id_from_url("https://google.com") == 1
